70263f3718e3f192d933bb203a93d7e6ed638e3e
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-12-14 20:52:41 -0400

    root hydration

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import { hydrateRoot } from 'react-dom/client'
 import { createInertiaApp } from '@inertiajs/inertia-react'
 import { InertiaProgress } from '@inertiajs/progress'
 
@@ -10,6 +10,6 @@ InertiaProgress.init()
 void createInertiaApp({
   resolve: async name => await import(`./pages/${name}`).then(module => module.default),
   setup({ el, App, props }) {
-    createRoot(el).render(<App {...props} />)
+    hydrateRoot(el, <App {...props} />)
   },
 })
