3d9a3e2dea1120f9f61c0e14c021a783119547a3
Gabriel Mariano Costa <gabrielmc15@outlook.com>
2022-12-20 22:37:33 -0400

    feat: routes for login and register

--- start/routes.ts
+++ start/routes.ts
@@ -3,6 +3,8 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/:id', 'LinkRedirectsController.show').as('link_redirect')
 
 Route.group(() => {
+  Route.resource('auth/register', 'RegistersController').only(['index', 'store'])
+  Route.resource('auth/login', 'LoginController').only(['index', 'store', 'destroy'])
   Route.inertia('/', 'Home/Index')
   Route.resource('links', 'LinksController')
 }).middleware(['inertia'])
